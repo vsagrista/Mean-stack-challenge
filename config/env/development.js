779de66fdebbe84a93b19fad96d3ef4804dd020d
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-08-02 12:57:38 +0200

    Setup mongodb

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://ds139685.mlab.com:39685/mean_stack_app_db' + '/mean-dev',
     options: {
-      user: '',
-      pass: ''
+      user: 'victor',
+      pass: 'victor'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
