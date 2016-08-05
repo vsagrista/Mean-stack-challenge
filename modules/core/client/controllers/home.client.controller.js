ecb9be6cbb3dab97f2fcdd6e956d4001a9f4a570
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-08-05 12:24:15 +0200

    Crud module for costumers

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,40 +6,40 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     $scope.authentication = Authentication;
     $scope.alerts = [
       {
-      	icon:'glyphicon glyphicon-user',
-      	color:'btn btn-success',
-      	total:'20,408',
-      	description:'TOTAL COSTUMERS'
+        icon:'glyphicon glyphicon-user',
+        color:'btn btn-success',
+        total:'20,408',
+        description:'TOTAL COSTUMERS'
       },
       {
-      	icon:'glyphicon glyphicon-calendar',
-      	color:'btn btn-primary',
-      	total:'8,382',
-      	description:'UPCOMING EVENTS'
+        icon:'glyphicon glyphicon-calendar',
+        color:'btn btn-primary',
+        total:'8,382',
+        description:'UPCOMING EVENTS'
       },
       {
-      	icon:'glyphicon glyphicon-edit',
-      	color:'btn btn-success',
-      	total:'527',
-      	description:'NEW COSTUMERS IN 24H'
+        icon:'glyphicon glyphicon-edit',
+        color:'btn btn-success',
+        total:'527',
+        description:'NEW COSTUMERS IN 24H'
       },
       {
-      	icon:'glyphicon glyphicon-record',
-      	color:'btn btn-info',
-      	total:'85,000',
-      	description:'EMAILS SENT'
+        icon:'glyphicon glyphicon-record',
+        color:'btn btn-info',
+        total:'85,000',
+        description:'EMAILS SENT'
       },
       {
-      	icon:'glyphicon glyphicon-eye-open',
-      	color:'btn btn-warning',
-      	total:'268',
-      	description:'FOLLOW UPS REQUESTED'
+        icon:'glyphicon glyphicon-eye-open',
+        color:'btn btn-warning',
+        total:'268',
+        description:'FOLLOW UPS REQUESTED'
       },
       {
-      	icon:'glyphicon glyphicon-flag',
-      	color:'btn btn-danger',
-      	total:'364',
-      	description:'REFERRALS TO MODERATE'
+        icon:'glyphicon glyphicon-flag',
+        color:'btn btn-danger',
+        total:'364',
+        description:'REFERRALS TO MODERATE'
       }
     ];
   }
