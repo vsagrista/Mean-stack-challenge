cdc52d21fa526a26b62f9a90af2067bb7be3e8e8
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-08-08 20:52:09 +0200

    UI angular modal working

--- modules/customers/client/controllers/list-customers.client.controller.js
+++ modules/customers/client/controllers/list-customers.client.controller.js
@@ -5,11 +5,36 @@
     .module('customers')
     .controller('CustomersListController', CustomersListController);
 
-  CustomersListController.$inject = ['CustomersService'];
+  CustomersListController.$inject = ['CustomersService', '$log', '$modal', '$scope'];
 
-  function CustomersListController(CustomersService) {
+  function CustomersListController(CustomersService, $log, $modal, $scope) {
     var vm = this;
-
+    
     vm.customers = CustomersService.query();
+    vm.modalUpdate = modalUpdate;
+
+    // Update costumer modal
+    
+    function modalUpdate(size, selectedCostumer) {
+      var modalInstance = $modal.open({
+        animation: true,
+        templateUrl: 'modules/customers/client/views/modal-customer.client.view.html',
+        controller: function ($scope, $modalInstance, customer){
+          $scope.customer = customer;
+        },
+        size: size,
+        resolve: {
+          customer: function () {
+            return selectedCostumer;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        vm.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    }
   }
 })();
